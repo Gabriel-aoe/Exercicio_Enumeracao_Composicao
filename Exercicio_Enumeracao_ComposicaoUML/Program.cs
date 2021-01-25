ef20dc4845268c53831f44f1657d7bb660ea9329
Gabriel Araujo <gabriel.araujo4002@gmail.com>
2021-01-24 22:48:25 -0200

    Save1

---
+++ Exercicio_Enumeracao_ComposicaoUML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio_Enumeracao_ComposicaoUML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

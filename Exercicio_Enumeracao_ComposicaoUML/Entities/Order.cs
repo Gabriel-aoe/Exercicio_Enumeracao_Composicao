8d504aef1d5f6974fc5acfb4fca4d1906c65a829
Gabriel Araujo <gabriel.araujo4002@gmail.com>
2021-01-24 23:30:49 -0200

    Created new class 'Order'

---
+++ Exercicio_Enumeracao_ComposicaoUML/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Exercicio_Enumeracao_ComposicaoUML.Entities.Enums;
+
+namespace Exercicio_Enumeracao_ComposicaoUML.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public void AddItem(OrderItem item)
+        {
+
+        }
+        public void RemoveItem(OrderItem item)
+        {
+
+        }
+    }
+}
